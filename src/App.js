cdfecb93ecc48ad869b97ecb4e34c7e06ffe8679
Cody Cogbill <ccogbill@btdtracker.com>
2024-07-15 14:53:06 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import './App.css';
-import profileImage from '/Users/codycogbill/Desktop/portfolio/ccportfolio/src/logo.svg'; // Adjust the path as needed
+import profileImage from './logo.svg'; // Adjust the path as needed
+import certImage from './images/cert.png'; // Adjust the path as needed
 import { IconButton } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
 
+Modal.setAppElement('#root'); // Set the root element for accessibility
+
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="header-content">
-          <h1>Cody Cogbill</h1>
+          <h1 className="header-name">Cody Cogbill</h1>
           <div className="profile-container">
             <div className="profile-background">
               <img src={profileImage} className="App-profile" alt="profile" />
             </div>
           </div>
-          <p>Full Stack Developer</p>
+          <p className="header-title">Full Stack Developer</p>
         </div>
         <div className="email-button-container">
           <IconButton
@@ -30,16 +44,20 @@ function App() {
       </header>
       <section className="App-section">
         <div className="panel white-panel">
-          <h2>I am a junior full-stack developer with a broad range of experience in various technologies. My passion for continuous learning and dedication to delivering results enable me to approach problems creatively and from diverse perspectives. I embrace challenges with humility and integrity, always striving to find innovative solutions. I am a proud graduate of the Flatiron School, where I specialized in full-stack engineering.</h2>
+          <h2>About Me</h2>
+          <p>I am a junior full-stack developer with a broad range of experience in various technologies. My passion for continuous learning and dedication to delivering results enable me to approach problems creatively and from diverse perspectives. I embrace challenges with humility and integrity, always striving to find innovative solutions. I am a proud <a href="#" onClick={openModal}>graduate of the Flatiron School</a>, where I specialized in full-stack engineering.</p>
         </div>
         <div className="panel black-panel">
-          <p>Below you can peak at my skills and technologies I've used in my engineering journey</p>
+          <h2>Skills</h2>
+          <p>Below you can peek at my skills and technologies I've used in my engineering journey:</p>
         </div>
         <div className="panel white-panel">
-          <p>If you are interested in starting a conversation about anything from video games, movies, to software engineering, you can email me at cody@happyinteractive.co</p>
-          <div className="panel black-panel">
-            <p> Or if you're in a reading mood, browse through my articles written about technologies that interested me during my time at Flatiron School <a href="https://medium.com/@codeman24">here</a>.</p>
-          </div>
+          <h2>Contact</h2>
+          <p>If you are interested in starting a conversation about anything from video games, movies, to software engineering, you can email me at <a href="mailto:cody@happyinteractive.co">cody@happyinteractive.co</a></p>
+        </div>
+        <div className="panel black-panel">
+          <h2>Articles</h2>
+          <p>Or if you're in a reading mood, browse through my articles written about technologies that interested me during my time at Flatiron School <a href="https://medium.com/@codeman24">here</a>.</p>
         </div>
       </section>
       <div className="ticker-wrapper">
@@ -79,6 +97,16 @@ function App() {
           <div className="ticker-item">GitLab</div>
         </div>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Certificate Modal"
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        <button onClick={closeModal} className="close-button">Close</button>
+        <img src={certImage} alt="Certificate" className="Modal-cert" />
+      </Modal>
     </div>
   );
 }
